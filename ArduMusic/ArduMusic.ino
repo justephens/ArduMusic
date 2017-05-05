6c3ebf34d80d06669d6e343c6a425a69c7c653df
Justin Stephens <jstephens2020@outlook.com>
2017-05-05 17:29:53 -0400

    R I P Buzzer #3
    When there were three buzzers, you would get a horrible sound after the first loop through a song due to the drift between timers (thanks to overhead from the code). Easy fix right? Just reference Micros() to keep the timers synced to when they should play, right?
    
    Well, nope. Using three buzzers means no internal timers left to power Micros(). The third buzzer is no longer with us.
    
    At least now we can rest, eh?

--- ArduMusic/ArduMusic.ino
+++ ArduMusic/ArduMusic.ino
@@ -3,18 +3,21 @@
 
 
 
-// Create three tone controllers
-Tone buzzer[3];
+// Create two tone controllers
+Tone buzzer[2];
+
+// Holds the time when the buzzers should play to
+unsigned long buzzer_target[2] = { 0, 0 };
 
 
 // The "cursor" for each of the buzzers
-unsigned int music_pos[3] = { 0, 0, 0 };
+unsigned int music_pos[2] = { 0, 0 };
 // Holds the length of the music
 unsigned int music_length = 9;
 
 
 // Defines the music
-Note Music[3][9] = {
+Note Music[2][9] = {
   {
     { NOTE_C4, 10 },
     { NOTE_D4, 10 },
@@ -24,7 +27,7 @@ Note Music[3][9] = {
     { NOTE_A4, 10 },
     { NOTE_B4, 10 },
     { NOTE_C5, 20 },
-    { NOTE_RST, 10 }
+    { NOTE_RST, 20 }
   },
   {
     { NOTE_E4, 10 },
@@ -35,18 +38,7 @@ Note Music[3][9] = {
     { NOTE_C5, 10 },
     { NOTE_D5, 10 },
     { NOTE_E5, 20 },
-    { NOTE_RST, 10 }
-  },
-  {
-    { NOTE_G4, 10 },
-    { NOTE_A4, 10 },
-    { NOTE_B4, 10 },
-    { NOTE_C5, 10 },
-    { NOTE_D5, 10 },
-    { NOTE_E5, 10 },
-    { NOTE_F5, 10 },
-    { NOTE_G5, 20 },
-    { NOTE_RST, 10 }
+    { NOTE_RST, 20 }
   }
 };
 
@@ -55,9 +47,8 @@ Note Music[3][9] = {
 void setup()
 {
   // Here we initiallize our three piezo buzzers
-  buzzer[0].begin(8);
-  buzzer[1].begin(9);
-  buzzer[2].begin(10);
+  buzzer[0].begin(9);
+  buzzer[1].begin(10);
 
   // Creates a Serial connection
   Serial.begin(9600);
@@ -67,18 +58,35 @@ void setup()
 
 void loop()
 {
-  for (unsigned int i = 0; i < 3; i++)
-  {
-    // If the buzzer is not playing
-    if (!buzzer[i].isPlaying())
-    {
-      // Play the next note, advance the cursor
-      buzzer[i].play(
-        pgm_read_word(Frequency + Music[i][music_pos[i]++].pitch),
-        Music[i][music_pos[i]].duration * 20);
+  // Taking a "snapshot" of the microseconds at the start of a loop
+  // will help us ensure the same value is sampled for both buzzers,
+  // thus removing drift in the time between buzzers
+  unsigned long micro_snapshot = micros();
 
-      // Wrap the cursor if necessary
+
+  // Iterate through both buzzers
+  for (unsigned int i = 0; i < 2; i++)
+  {
+
+    // Check if it's time for the buzzer to stop
+    if (micro_snapshot >= buzzer_target[i])
+    {
+      
+      // Increment the music pointer, wrap it to begin if necessary
+      music_pos[i]++;
       if (music_pos[i] >= music_length) music_pos[i] = 0;
+
+      
+      // Stop buzzer, play next note (as long as it's not a rest)
+      buzzer[i].stop();
+      if (Music[i][music_pos[i]].pitch != pgm_read_word(Frequency + NOTE_RST))
+        buzzer[i].play(pgm_read_word(Frequency + Music[i][music_pos[i]].pitch));
+
+
+      // Set the time to stop playing
+      buzzer_target[i] += Music[i][music_pos[i]].duration * 50000;
+      
     }
+    
   }
 }
