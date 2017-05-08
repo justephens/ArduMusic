d767040e2ac1a05f8e8ae79405e5f69e885138f5
Justin Stephens <jstephens2020@outlook.com>
2017-05-07 22:22:30 -0400

    Setup to match the new Serial protocol. Cleaned up console output and added a summary that prints at the end of a music transfer

--- ArduMusic.py
+++ ArduMusic.py
@@ -7,12 +7,16 @@ import SerialHelper
 # and then form a connection with the given device
 device = SerialHelper.select_device()
 Arduino = SerialHelper.device_begin(device)
+print("\n...\n")
+
 
 
 
 # Asks the user for a file to load music from, and
 # loads it into a list of tuples (pitch, duration)
 Music = MuseScore.read_file()
+print("\n...\n")
+
 
 # Loop through the musical tuples and build a byte
 # array that can be sent to the Arduino
@@ -23,27 +27,29 @@ for i in Music[2:]:
 
 
 
+
+
 # Write 255 to the Arduino to begin a transmission
 Arduino.write(bytes([255]))
 
-
-
-# Once the Arduino recieves the starting opcode, it
-# will send back the max length of the song
+# The Arduino will send back the maximum notes it
+# can handle (this is per-buzzer). From this we
+# decide how many we are actually going to send
 Max_notes       = int(SerialHelper.device_readline(Arduino))
-print(str(Max_notes))
+Num_notes       = Max_notes if int(len(output)/2) > Max_notes else int(len(output)/2)
+
+Num_buzzers     = int(SerialHelper.device_readline(Arduino))
+
+# Send a message dictating how many notes will be sent
+Arduino.write(SerialHelper.pack_short(Num_notes))
+
+# Send the data up to the requested size by the Arduino
+# We multiply by 2 because each note takes up 2 bytes
+Arduino.write(output[0:Num_notes*2])
 
 
 
-# After recieving information about the Arduino, we
-# send back a message confirming the number of notes
-Arduino.write(SerialHelper.pack_short(9))
-
-print(output[0:Max_notes])
-Arduino.write(output[0:Max_notes])
-
-
-#Arduino.write("R".encode('utf-8'))
-
-#MuseScore.read_file()
-
+# Print a nice overview at the end
+print("Music Transfer Complete:")
+print("  Arduino with " + str(Num_buzzers) + " buzzers")
+print("  " + str(Num_notes) + " notes transferred")
