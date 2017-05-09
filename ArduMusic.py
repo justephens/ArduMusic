15a2dab0a09345e1e28db30f36cdfba095b941bb
Justin Stephens <jstephens2020@outlook.com>
2017-05-08 20:06:42 -0400

    Can now send data for two buzzers. The code is really messy and needs to be cleaned.

--- ArduMusic.py
+++ ArduMusic.py
@@ -14,17 +14,33 @@ print("\n...\n")
 
 # Asks the user for a file to load music from, and
 # loads it into a list of tuples (pitch, duration)
-Music = MuseScore.read_file()
+Music_Raw = MuseScore.read_file()
+Music_b1 = Music_Raw[0]
+Music_b2 = Music_Raw[1]
+
+print("BUZZER 1:")
+for i in Music_b1:
+    print ("  " + str(i[0]) + " " + str(i[1]))
+
+print("BUZZER 2:")
+for i in Music_b2:
+    print ("  " + str(i[0]) + " " + str(i[1]))
+    
 print("\n...\n")
 
 
 # Loop through the musical tuples and build a byte
 # array that can be sent to the Arduino
-output = bytes([Music[1][0]]) + bytes([Music[1][1]])
-for i in Music[2:]:
-    output += bytes([i[0]])
-    output += bytes([i[1]])
+output_b1 = bytes(Music_b1[1])
+for i in Music_b1[2:]:
+    output_b1 += bytes(i)
+    
+output_b2 = bytes(Music_b2[1])
+for i in Music_b2[2:]:
+    output_b2 += bytes(i)
 
+print(output_b1)
+print(output_b2)
 
 
 
@@ -36,16 +52,24 @@ Arduino.write(bytes([255]))
 # can handle (this is per-buzzer). From this we
 # decide how many we are actually going to send
 Max_notes       = int(SerialHelper.device_readline(Arduino))
-Num_notes       = Max_notes if int(len(output)/2) > Max_notes else int(len(output)/2)
-
 Num_buzzers     = int(SerialHelper.device_readline(Arduino))
+Num_notes       = Max_notes if int(len(output_b1)/2) > Max_notes else int(len(output_b1)/2)
+
 
 # Send a message dictating how many notes will be sent
+# to the Arduino
 Arduino.write(SerialHelper.pack_short(Num_notes))
 
-# Send the data up to the requested size by the Arduino
-# We multiply by 2 because each note takes up 2 bytes
-Arduino.write(output[0:Num_notes*2])
+# Next we send the note output for Buzzer one
+Arduino.write(output_b1[0:Num_notes*2])
+
+
+# Send a message dictating how many notes will be sent
+# to the Arduino
+Arduino.write(SerialHelper.pack_short(Num_notes))
+
+# Next we send the note output for Buzzer one
+Arduino.write(output_b2[0:Num_notes*2])
 
 
 
